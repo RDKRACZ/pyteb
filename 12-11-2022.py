123e62fa067c540b2cdbcc289555c814fc3e0c4b
RDKRACZ <rdkracz@gmail.com>
2022-11-12 12:09:36 +0100

    1

---
+++ 12-11-2022.py
@@ -0,0 +1,88 @@
+# while True:
+#     a = int(input("liczba: "))
+#     try:
+#         print(float(a) / 2)
+#     except ValueError as e:
+#         print(e)
+
+
+# from random import randint
+# proba = 0
+# y = 0
+# a = randint(1, 10)
+# while a != y:
+#     y = int(input('zgadnij liczbe: '))
+#     if type(y) is not int:
+#         print('test') 
+#     print('pudlo')
+#     proba += 1
+# print(f'zgadles za {proba} razem')
+
+
+# from random import randint
+# def wypiszLiczby(a):
+#     print(a)
+#     print(a+10)
+
+# wypiszLiczby(2)
+
+
+# from random import randint
+
+# def czy_zgadles(a):
+#     if a == randint(0,3):
+#         return True
+#     else:
+#         return False
+
+# x=False
+# while x == False:
+#     a=input()
+#     x=czy_zgadles(a)
+
+
+# lista = "zwykly,, tekst,   zwykly tekst"
+# print(lista[0])
+# print(lista.split(","))
+# print(lista[:-1])
+# print(lista.replace(',,',','))
+
+# kolekcja = ['a','b','c']
+# for i in range(len(kolekcja)):
+#     if i == 2:
+#         kolekcja[i] = 'tekst'
+
+# print(kolekcja)
+
+
+zero =  ["  xx  ",
+         " x  x ",
+         "x    x",
+         "x    x",
+         "x    x",
+         " x  x ",
+         "  xx  "]
+for i in zero:
+    print(i)
+
+
+jeden = ["    xx",
+         "   xxx",
+         "  x xx ",
+         "    xx ",
+         "    xx ",
+         "    xx ",
+         "   xxxx"]
+for i in jeden:
+    print(i)
+
+
+for i in range(7):
+    tekst=zero[i]+" "+jeden[i]
+    print(tekst)
+
+
+a = input("podaj liczbe: ")
+for i in range(len(a)):
+    if a[i] == "1":
+        tekst += jeden[j]
--- calc.py
+++ calc.py
@@ -4,8 +4,20 @@ while True:
     a=str(input("wprowadź typ działania, lub 'x' aby zakończyć: "))
     if a == 'x':
         break
-    b = float(input("liczba nr jeden: "))
-    c = float(input("liczba nr dwa: "))
+    # if not a == "-" or "+" or "/" or "*" or "**" or "%":
+    #     print("Niepoprawny znak działania")
+    #     continue
+    try:
+        b = float(input("liczba nr jeden: "))
+    except ValueError as e:
+        print("To nie liczba")
+        continue
+    try:
+        c = float(input("liczba nr dwa: "))
+    except ValueError as e:
+        print("To nie liczba")
+        continue
+
     if a == '-':
         print(b - c)
     if a == '+':
